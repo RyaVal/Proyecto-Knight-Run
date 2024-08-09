a272ef6abdefd29265744fea5fc1523e31ce5708
SusanHidalgoG <pamelahidalgogonzalez@gmail.com>
2024-08-08 22:26:58 -0600

    Sound Manager
    Sonido de Theme funciona a la perfeccion, falta perfeccionar los otros

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+
+public class Sound
+{
+    [SerializeField]
+    string name;
+
+    [SerializeField]
+    AudioClip audio;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public AudioClip GetAudio()
+    {
+        return audio;
+    }
+}
